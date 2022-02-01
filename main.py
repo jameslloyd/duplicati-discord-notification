ea6b750218dd2105de8f4c78b5bc639768d4ea9a
James Lloyd <james.lloyd@gmail.com>
2022-02-01 15:33:50 +0000

    enable duplicati-monitor pass through

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, request
+import requests
 from discord_webhook import DiscordWebhook, DiscordEmbed
 
 colour = {}
@@ -10,6 +11,14 @@ colour['FATAL'] = 'FF0000'
 
 
 app = Flask(__name__)
+
+def sizeof_fmt(num, suffix="B"):
+    num = int(num)
+    for unit in ["", "Ki", "Mi", "Gi", "Ti", "Pi", "Ei", "Zi"]:
+        if abs(num) < 1024.0:
+            return f"{num:3.1f}{unit}{suffix}"
+        num /= 1024.0
+    return f"{num:.1f}Yi{suffix}"
 
 @app.route("/report", methods=['POST'])
 def report():
@@ -25,14 +34,19 @@ def report():
                     i = item.split(': ')
                     output[i[0]]=i[1]
             webhook = DiscordWebhook(url=webhookurl)
+            size = sizeof_fmt(output["SizeOfExaminedFiles"])
             title = f'Duplicati job {name} {output["MainOperation"]} {output["ParsedResult"]}'
-            description=f'{output["MainOperation"]} started at {output["BeginTime"]}\n{output["MainOperation"]} took {output["Duration"]}\nNumber of files {output["ExaminedFiles"]}\nNumber of modified files {output["ModifiedFiles"]}\nSize of backup {output["SizeOfExaminedFiles"]}'
+            description=f'{output["MainOperation"]} started at {output["BeginTime"]}\n{output["MainOperation"]} took {output["Duration"]}\nNumber of files {output["ExaminedFiles"]}\nNumber of modified files {output["ModifiedFiles"]}\nSize of backup {size}'
             footer = f'{output["ParsedResult"]}'
             color = colour[output["ParsedResult"]]
             embed = DiscordEmbed(title=title,description=description,color=color)
             embed.set_footer(text=footer)
             webhook.add_embed(embed)
             response = webhook.execute()
+        if request.args.get('duplicatimonitor'):
+            postdata = {'message': message}
+            requests.post(request.args.get('duplicatimonitor'), data = postdata)
+
     return '{}'
 
 if __name__ == "__main__":
